346d7ca2bd891e198198640730b63ae38aec28b2
wp <infopurpose@mail.ru>
2021-08-30 20:55:01 +0600

    Update

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,11 +1,11 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/whitepowder/otushw2/hw02_unpack_string
 
 go 1.16
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	mvdan.cc/gofumpt v0.1.1 // indirect
 )
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,48 +9,70 @@ import (
 )
 
 var (
+	// ErrInvalidString is cool.
 	ErrInvalidString = errors.New("invalid string")
-	ErrNo            = errors.New("")
 	strBuilder       strings.Builder
-	runeArray        []rune
-	splitedStr       []string
 )
 
 func Unpack(inputStr string) (string, error) {
+	var runeArray []rune
+	var splittedStr []string
+	strBuilder.Reset()
+	splittedStr = strings.Split(inputStr, "")
 	runeArray = []rune(inputStr)
 	for i := 0; i < len(runeArray); i++ {
 		switch {
 		case unicode.IsDigit(runeArray[i]) && i == 0:
 			return "", ErrInvalidString
-		case unicode.IsDigit(runeArray[i]) && unicode.IsDigit(runeArray[i-1]):
+		case unicode.IsDigit(runeArray[i]) && unicode.IsDigit(runeArray[i-1]) && runeArray[i-2] != 92:
 			return "", ErrInvalidString
 		default:
-			BuiltString(inputStr, i)
+			selector(runeArray, i, splittedStr)
 		}
 	}
-	return strBuilder.String(), ErrNo
+	return strBuilder.String(), nil
 }
 
-func getRepeatValue(str string, position int) (int, error) {
-	splitedStr = strings.Split(str, "")
+func selector(runeArray []rune, i int, splittedStr []string) {
 	switch {
-	case unicode.IsDigit(runeArray[position]):
-		return strconv.Atoi(splitedStr[position])
+	case i < len(runeArray)-1 && unicode.IsLetter(runeArray[i]) && runeArray[i+1] == 48:
+		builder(runeArray[i], 1)
+	case i > 2 && runeArray[i-1] == 92 && runeArray[i-2] == 92 && runeArray[i-3] == 92:
+		fmt.Println("..")
+		builder(runeArray[i], 2)
+	case unicode.IsDigit(runeArray[i]):
+		digit(runeArray, i, splittedStr)
+	case runeArray[i] == 92:
+		escape(runeArray, i)
 	default:
-		return 1, ErrNo
+		builder(runeArray[i], 2)
 	}
 }
 
-func BuiltString(str string, position int) {
-	repeatValue, ErrNo := getRepeatValue(str, position)
+func digit(runeArray []rune, i int, splittedStr []string) {
 	switch {
-	case position != 0:
-		if !unicode.IsDigit(runeArray[position-1]) {
-			strBuilder.WriteString(strings.Repeat(splitedStr[position-1], repeatValue))
-		}
-	case (position+1 == len(runeArray)) && !unicode.IsDigit(runeArray[position]):
-		strBuilder.WriteString(strings.Repeat(splitedStr[position], repeatValue))
-	default:
-		fmt.Print(ErrNo)
+	case i == 1 && runeArray[i-1] == 92:
+		builder(runeArray[i], 2)
+	case runeArray[i-1] != 92:
+		repeatValue, _ := strconv.Atoi(splittedStr[i])
+		builder(runeArray[i-1], repeatValue)
+	case runeArray[i-1] == 92 && runeArray[i-2] == 92:
+		repeatValue, _ := strconv.Atoi(splittedStr[i])
+		builder(runeArray[i-1], repeatValue)
+	}
+}
+
+func escape(runeArray []rune, i int) {
+	switch {
+	case i == 0:
+		builder(runeArray[i+1], 2)
+	case i > 0 && runeArray[i-1] != 92:
+		builder(runeArray[i+1], 2)
+	}
+}
+
+func builder(r rune, repeat int) {
+	for i := 0; i < repeat-1; i++ {
+		strBuilder.WriteRune(r)
 	}
 }
