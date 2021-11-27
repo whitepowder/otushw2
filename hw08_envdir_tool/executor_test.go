a52308997aaddb613d122a1697607bd0d14fbba4
wp <infopurpose@mail.ru>
2021-11-27 20:09:31 +0600

    HW8 is done

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -7,14 +7,13 @@ import (
 )
 
 func TestRunCmd(t *testing.T) {
-	t.Run("Expected 0", func(t *testing.T) {
+	t.Run("Expected_0", func(t *testing.T) {
 		env, _ := ReadDir("testdata/env")
-		cmd := []string{"./go-envdir", "/home/wp/otushw2/hw08_envdir_tool/testdata/env", "/bin/bash", "/home/wp/otushw2/hw08_envdir_tool/testdata/echo.sh", "arg1=1", "arg2=2"}
+		cmd := []string{"/bin/bash", "/home/wp/otushw2/hw08_envdir_tool/testdata/echo.sh", "arg1=1", "arg2=2"}
 		exitCode := RunCmd(cmd, env)
 		require.Equal(t, 0, exitCode)
 	})
-
-	t.Run("Expected 1", func(t *testing.T) {
+	t.Run("Expected_1", func(t *testing.T) {
 		env, _ := ReadDir("testdata/env")
 		cmd := []string{"", "", "", ""}
 		exitCode := RunCmd(cmd, env)
